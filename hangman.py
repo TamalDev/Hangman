9013da38e938d08c1ebeb11e83be2f7ff49b4824
Andres Bulla <andresbulla08@gmail.com>
2021-11-08 14:26:42 -0500

    Programa de Hangman terminado

--- hangman.py
+++ hangman.py
@@ -20,28 +20,54 @@ BIENVENIDO AL JUEGO DEL AHORCADO!!
 
 """)
 
-print("""A continuación ingresa la letra que consideres hace parte de la palabra secreta: 
-
-""")
-
-from os import replace
 import random
-import io
 
-def palabra_aleatoria(palabras):
+def hangman():
+    intentos=5
+    acierto=0
+    lista=[]
+    final=""
 
-    with io.open ("palabras.txt", "r", encoding='utf-8') as f:
-        nueva_palabra = list(f)
+    with open ("palabras.txt", "r", encoding='utf-8') as f:
+        for i in f:
+            lista.append(i.rstrip("\n"))
 
-    return random.choice(nueva_palabra).strip()
+    palabra_aleatoria = random.choice(lista)
 
-for i in range(1):
-    print(palabra_aleatoria(".Hangman/palabras.txt"))
+    adivina =["_" for i in palabra_aleatoria]
+    print(""" 
+        Bienvenido/a al juego del ahorcado 
+        la palabra que debes adivinar es """  +"_ "*len(palabra_aleatoria)
+        +"Tienes " +str(intentos)+" intentos")
+    while(True):
 
-def run():
-    pass
+        letra=input("Ingresa una letra ").lower()
+        if(len(letra)>=2 or letra.isalpha==False):
+            letra=""
+            print(" Has ingresado un caracter inválido")
+            continue
 
+        for count, i in enumerate(palabra_aleatoria):
+            if letra == i:
+                adivina[count]=letra
+                acierto+=1
+        print("acertaste "+ str(acierto)+" veces la letra "+ letra)
+        acierto=0
+        if letra not in palabra_aleatoria:
+            intentos -=1
+            print("Te has equivocado, te quedan "+ str(intentos)+ " intentos")
+        
+        for i in adivina:
+            print(i , end=" ")
+            final+=i
+        if final == palabra_aleatoria:
+            print("Has ganado, la palabra correcta es "+final)
+            break
+        if intentos ==0:
+            print("Has perdido, mejor suerte la próxima. La palabra correcta era "+ palabra_aleatoria)
+            break
+        final=""        
 
 
 if __name__ == '__main__':
-    run()
+    hangman()
